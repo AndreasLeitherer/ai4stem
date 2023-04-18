aa10bc469e573afabfefce336933b0df5244ee9b
Andreas Leitherer <leitherer@fhi-berlin.mpg.de>
2023-04-18 14:42:06 +0200

    Updated unsupervised notebook and module; small correction in readme

--- ai4stem/utils/utils_unsupervised.py
+++ ai4stem/utils/utils_unsupervised.py
@@ -1,13 +1,22 @@
 import umap
+from ai4stem.utils.utils_data import load_pretrained_model
 
 class UnsupervisedLearning():
+    """
+    UNDER CONSTRUCTION.
+    Class for generalizing unsupervised learning (here dimension reduction / manifold learning).
+    In the current implementation, only umap is contained.
+    """
     
-    
-    def __init__(self, method='umap', params={'nn': 200}):
+    def __init__(self, method='umap', params={'n_neighbors': 200,
+                                              'min_dist': 0.9,
+                                              'metric': 'euclidean',
+                                              'n_components': 2},
+                 mapper=None):
         
         self.method = method
         self.params = params
-        pass
+        self.mapper = None
     
     def fit(self, data):
         if self.method == 'umap':
@@ -17,3 +26,22 @@ class UnsupervisedLearning():
         if self.method == 'umap':
             embedding = umap.fit_transform(data, **self.params)
             return embedding
+    def transform(self, data):
+        if self.method == 'umap':
+            embedding = self.mapper.transform(data)
+            return embedding
+        
+        
+def unsupervised_analysis(image, window_size=100, model=None, n_iter=100, method='umap', params=None):
+    
+    if model == None:
+        model = load_pretrained_model()
+    if params == None:
+        params = {'n_neighbors': 200,
+                  'min_dist': 0.9,
+                  'metric': 'euclidean',
+                  'n_components': 2}
+        
+    
+        
+    
